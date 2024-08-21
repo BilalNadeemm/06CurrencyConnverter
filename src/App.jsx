2992bc8e406f3594707c8a13499bc9936a3f71f3
BilalNadeem <bilalnadeemm2003@gmail.com>
2024-08-21 21:55:14 +0500

    final

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import UseCurrencyInfo from './hooks/hooks';
 import InputBox from './components/InputBox';
 
 function App() {
-    const [amount, setAmount] = useState(0);
+    const [amount, setAmount] = useState("");
     const [from, setFrom] = useState("USD");
     const [to, setTo] = useState("PKR");
-    const [convertedAmount, setConvertedAmount] = useState(0);
+    const [convertedAmount, setConvertedAmount] = useState("");
 
     const currencyInfo = UseCurrencyInfo(from);
     const options = Object.keys(currencyInfo);
@@ -26,10 +26,10 @@ function App() {
         <div
             className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
             style={{
-                backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
+                backgroundImage: `url('https://images.pexels.com/photos/259191/pexels-photo-259191.jpeg?auto=compress&cs=tinysrgb&w=600')`,
             }}
         >
-          <h1 className='text-6xl text-orange-600'>Currency Generator</h1>
+          <h1 className='text-7xl font-semibold text-green-900 text-center  '> GHAZI Currency Generator</h1>
             <div className="w-full">
                 <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
                     <form
@@ -51,7 +51,7 @@ function App() {
                         <div className="relative w-full h-0.5">
                             <button
                                 type="button"
-                                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-orange-600 text-white px-2 py-0.5"
+                                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-green-600 text-white px-2 py-0.5"
                                 onClick={swap}
                             >
                                 swap
@@ -67,7 +67,7 @@ function App() {
                                 amountDisable
                             />
                         </div>
-                        <button type="submit" className="w-full bg-orange-600 text-white px-4 py-3 rounded-lg">
+                        <button type="submit" className="w-full bg-green-600 text-white px-4 py-3 rounded-lg">
                             Convert {from.toUpperCase()} to {to.toUpperCase()}
                         </button>
                     </form>
